669d9912d4ef48e102258ed2a6c4c0671383e124
TuNombre <davogreg12@gmail.com>
2025-04-09 16:16:19 -0500

    Primer commit

---
+++ src/App.java
@@ -0,0 +1,19 @@
+public class App {
+    public static void main(String[] args) throws Exception {  //los metodos staticos solo pueden llamar a metodos estaticos, antes en el de abajo no estaba estatico, pero se cambio para que funcione
+        
+        int n = 5;
+        int rresultadoFinal = factorial( n );
+        System.out.println("Resultado: " + rresultadoFinal);
+    }
+
+    public static int factorial(int n){
+        if(n==0){
+            System.out.println("Alcanze el caso base");
+            return 1;
+        } 
+
+        int resultadoParcial = n * factorial(n-1);
+        System.out.println("Calculando factorial de: " + n + " * " + "factorial(" + (n-1) + " -1)");
+        return resultadoParcial;
+    }
+}
