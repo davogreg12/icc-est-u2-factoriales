994bc880745c307f14e952aacb94b4c685088c3f
TuNombre <davogreg12@gmail.com>
2025-04-10 19:12:38 -0500

    actualizado

--- src/App.java
+++ src/App.java
@@ -16,14 +16,31 @@ public class App {
         System.out.println("Calculando factorial de: " + n + " * " + "factorial(" + (n-1) + " -1)");
         return resultadoParcial; */
 
-        ejerciciosRecursivos ejercicios = new ejerciciosRecursivos();
+
+
+        /*ejerciciosRecursivos ejercicios = new ejerciciosRecursivos();
 
         int resultadoFibonacci = ejercicios.fibonacci(5); 
         System.out.println("Fibonacci de 10: " + resultadoFibonacci);
 
 
         int resultadoSuma = ejercicios.sumaConsecutivos(5); 
-        System.out.println("Suma de consecutivos hasta 5: " + resultadoSuma);
+        System.out.println("Suma de consecutivos hasta 5: " + resultadoSuma); */
     
+
+        System.out.println(); 
+        ejerciciosRecursivos ejercicios = new ejerciciosRecursivos(); 
+        int base = 2;   //Estas tres lineas sirven para poder nostros dar valores  a la base y exponente, sin la parte de resultado no podr[ia funcionar la [ultima linea]]                                                    
+        int exponente = 3;
+        int resultado = ejercicios.getPotencia(base, exponente); 
+        System.out.println("El resultado es "+ base +"^"+ exponente +"= " + resultado); 
+        ////
+        
+        ejerciciosRecursivos ejercicioultimo = new ejerciciosRecursivos();
+        int n = 125;
+        int resultadoUltimo = ejercicioultimo.ultimoejerc(n);
+        System.out.println("El resultado es: " + resultadoUltimo); 
+
+
     }
 }
--- src/ejerciciosRecursivos.java
+++ src/ejerciciosRecursivos.java
@@ -1,6 +1,6 @@
 public class ejerciciosRecursivos {
 
-    public int fibonacci(int n) {
+    /*public int fibonacci(int n) {
         if (n <= 1) {
             return n;
         }
@@ -14,5 +14,32 @@ public class ejerciciosRecursivos {
         int resultadoParcial = sumaConsecutivos(n-1);
         int resultado = n + resultadoParcial;
         return resultado;
+    } */
+
+
+
+    public int getPotencia (int base, int exponente){
+        if (exponente==1){
+            return base;
+        }
+        int resultadoPotencia = getPotencia(base, exponente-1);
+        int resultado = base * resultadoPotencia;
+        return resultado;
+    }
+
+
+    ////
+    
+    public int ultimoejerc (int n){
+        if (n<10){
+            return n;
+        }
+        int ultimoDigito = n%10;
+        int num = n /10;
+        return ultimoDigito + ultimoejerc(num);
     }
 }
+
+// git add.
+//git commit -m
+//git push
